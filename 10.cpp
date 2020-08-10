a0d036ec1482a526e10a3b4fdd3d70b77309548d
sagar yadav <40927722+rahulyadav97185966@users.noreply.github.com>
2020-08-10 16:14:49 +0530

    Create 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,11 @@
+//Excel sheet Column number
+
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int ans = 0,n = s.length()-1;
+        for(int i = n;i>=0;i--)
+            ans+=pow(26,n-i)*(s[i]-'A'+1);      
+        return ans;
+    }
+};
